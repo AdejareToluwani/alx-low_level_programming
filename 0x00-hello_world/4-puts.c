5b1b3f7981867a24690a6beef3ee8c88346cee71
AdejareToluwani <Adejaretobiloba09@gmail.com>
2023-02-16 21:59:11 -0800

    dddd

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *main - Entry Point
+ *Return : Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	puts("\" programming is like building a multilingual puzzle");
+	return(0);
+}
