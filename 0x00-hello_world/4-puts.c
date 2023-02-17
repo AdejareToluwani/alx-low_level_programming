e8847b1b04597958915514714de7563a70722df7
AdejareToluwani <Adejaretobiloba09@gmail.com>
2023-02-16 22:27:06 -0800

    d-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *main - Entry Point
- *Return : Always 0 (SUCCESS)
+ * main - prints the given line using puts func.
+ * Return: 0 (means success)
  */
-
 int main(void)
 {
-	puts("\" programming is like building a multilingual puzzle");
-	return(0);
-}
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}       
