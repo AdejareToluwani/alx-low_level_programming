e85447c51bc975aec5adc7ae506201a399275d53
AdejareToluwani <Adejaretobiloba09@gmail.com>
2023-02-20 02:22:34 -0800

    b_16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: base16
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (i = 0; i < 6; i++)
+		putchar(i + 'a');
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,3 +3,4 @@ alphabet in upper and lowercase
 exclude q and e
 all single digit of base 10
 lower case alphabet in reverse
+base 16
