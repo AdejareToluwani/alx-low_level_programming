9aef6e5c1ca40402a35d5eff49f2050dff58ff9a
AdejareToluwani <Adejaretobiloba09@gmail.com>
2023-02-21 12:42:21 -0800

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Display alphabet X10
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char alph;
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+			_putchar(c);
+
+		_putchar('\n');
+	}
+}
