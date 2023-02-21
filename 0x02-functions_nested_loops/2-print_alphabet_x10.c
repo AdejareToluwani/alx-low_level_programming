37edfe12f90759762a32c0aeadf83778a60b6bf1
AdejareToluwani <Adejaretobiloba09@gmail.com>
2023-02-21 12:58:41 -0800

    x10 corrected

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	char alph;
 	int num;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num < 10; num++)
 	{
 		for (alph = 'a'; alph <= 'z'; alph++)
 			_putchar(alph);
