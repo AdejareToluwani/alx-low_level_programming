0ce905fc79522a808e2fb1216924067bdc803f75
AdejareToluwani <Adejaretobiloba09@gmail.com>
2023-02-23 19:48:54 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul -finds/multiply the product of two ints
+ * @a:int 1
+ * @b: int 2
+ * Return: var result displays output
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
