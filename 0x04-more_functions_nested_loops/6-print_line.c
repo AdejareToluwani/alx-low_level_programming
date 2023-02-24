aab632caea1b6af7e0debc598a33379acb07744d
AdejareToluwani <Adejaretobiloba09@gmail.com>
2023-02-23 20:59:30 -0800

    char

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+
+#include "main.h"
+
+/**
+ * print_line - print n underscore characters
+ * @n: number of times to print _
+ */
+void print_line(int n)
+{
+	int set;
+
+	if (n > 0)
+	{
+		for (set = 0; i < n; set++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
+
