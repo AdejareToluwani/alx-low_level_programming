8425c38b6fcf35eff2c92a8ee1492b037df63613
AdejareToluwani <Adejaretobiloba09@gmail.com>
2023-02-27 21:03:51 -0800

    r_newline

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - This function prints a reverse string
+ * @s: This is the input string
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (len)
+		_putchar(s[--len]);
+	_putchar('\n');
