3dfea65c835fa118f09f1083e23502826232a2e0
AdejareToluwani <adejaretobiloba09@gmail.com>
2023-03-02 19:00:53 -0800

    c_strings_betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#include 'main.h'
+#include "main.h"
 
 /**
  * * _strncat - Concatenates two strings using at most
@@ -11,11 +11,19 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int i = 0, j = 0;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
