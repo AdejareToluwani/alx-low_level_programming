acaadf7108cdc8f707eebb63b6c105ba73380434
AdejareToluwani <adejaretobiloba09@gmail.com>
2023-03-24 12:25:58 -0700

    initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints name
+ * @name: pointer to the name
+ * @f: pointer to the function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
+
